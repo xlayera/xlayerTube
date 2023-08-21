50477ba7eea4044ef9133d1d61092c934d8e466a
Ximena Layera <xlayera22@gmail.com>
2023-08-21 09:26:56 -0400

    Delete database.js

--- server/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const URI = "mongodb+srv://xlayer:5OC6xWKcajJ7U26X@cluster0.ycsorpl.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
-    console.log('Mongodb connected');
-
-}).catch(error => {
-    console.log('Error to connect mongodb', error);
-})
